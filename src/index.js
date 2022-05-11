aeffae00d43c6cdd393b60916aaa379d40589e09
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-05-11 10:11:46 -0500

    rm useless code

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom/client';
 import TTV from "./components/TTV"
 
 
-import { BrowserRouter } from "react-router-dom";
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<TTV />);
