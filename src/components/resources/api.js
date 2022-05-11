024c37b5b8a294b47c82b94f9b1e4bbc6771251b
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-05-10 22:38:05 -0500

    apiUtils

--- src/components/resources/api.js
+++ src/components/resources/api.js
@@ -1,27 +1,14 @@
-/**
-     * Makes a call against a given endpoint using a specific method. 
-     * 
-     * Returns a Promise with the Request() object per fetch documentation.
-     * 
-     */
+const oauth = require("../OAuth/utils.js");
 
- function makeCall(url, method="GET"){
-    return new Promise((resolve, reject)=>{
-        if(this.isAuthenticated()){
-            let headers={
-                'Content-Type':'application/json',
-                'Authorization': `Bearer ${this.state.token}`
-            }
 
-            fetch(url,
-                {
-                    method,
-                    headers,
-                })
-                .then(response=>resolve(response))
-                .catch(e=>reject(e))
-        }else{
-            reject('Unauthorized')
+const requestHeader = (auth) => {
+    if (oauth.eval.authenticated(auth)) {
+        return {
+            'Content-Type':'application/json',
+            'Authorization': `Bearer ${auth.token}`
         }
-    })
+    } else return false;
 }
+
+
+module.exports = requestHeader;
